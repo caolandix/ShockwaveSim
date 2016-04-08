4eabddecb29fdd4646a744961437da9855972407
caolan <caolan@aeshna.net>
2016-04-07 23:10:02 -0700

    - Adding the SymbolicC++ Math library - Some minor class changes

--- shockregion.h
+++ shockregion.h
@@ -9,6 +9,7 @@ public:
 
     // copy overload
     ShockRegion &operator=(const ShockRegion &);
+    bool operator==(const ShockRegion &);
 
     double Temperature() const { return m_Temperature; }
     void Temperature(const float val) { m_Temperature = val; }
